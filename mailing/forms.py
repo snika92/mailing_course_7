840a91676f3d8346c58bbf6f652c1def19576bd0
Veronika Surkova <surkova_nika92@mail.ru>
2025-03-31 15:52:07 +0300

    Add MessageForm, ClientForm, MailingForm

---
+++ mailing/forms.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.forms import (BooleanField, CheckboxSelectMultiple, ModelForm,
+                          ModelMultipleChoiceField, SelectMultiple)
+
+from mailing.models import Client, Mailing, Message
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class MessageForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Message
+        exclude = ["created_at", "updated_at", "owner"]
+
+
+class ClientForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Client
+        exclude = ["created_at", "updated_at", "owner"]
+
+
+class MailingForm(StyleFormMixin, ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        the_user = kwargs.pop("user", None)
+        super(MailingForm, self).__init__(*args, **kwargs)
+        qs_clients = Client.objects.filter(owner=the_user)
+        if the_user is not None:
+            self.fields["message"].queryset = the_user.message_set.all()
+            self.fields["clients"] = ModelMultipleChoiceField(
+                queryset=qs_clients, widget=SelectMultiple, label="Клиенты"
+            )
+
+    class Meta:
+        model = Mailing
+        exclude = ["started_at", "finished_at", "owner", "status_mail"]
