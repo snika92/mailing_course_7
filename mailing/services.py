91c3561c8da95da55c2e99eb050a42334132a0d0
Veronika Surkova <surkova_nika92@mail.ru>
2025-04-07 18:10:40 +0300

    Add send_mail, update_status

---
+++ mailing/services.py
@@ -0,0 +1,36 @@
+from django.utils import timezone
+from django.core.cache import cache
+from config.settings import CACHE_ENABLED, EMAIL_HOST_USER
+from django.core.mail import send_mail
+from mailing.models import AttemptToSend
+
+
+def send_email(mailing, client):
+    """Функция отправки сообщений пользователям и занесение данных по результатам рассылки"""
+    try:
+        mail_response = send_mail(
+            subject=mailing.message.theme,
+            message=mailing.message.body,
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[client.email],
+            fail_silently=False,
+        )
+    except Exception as e:
+        mail_response = e
+
+    AttemptToSend.objects.create(
+        status_log=AttemptToSend.SUCCESS if mail_response == 1 else AttemptToSend.FAILED,
+        server_response=mail_response if mail_response != 1 else "",
+        mailing_list=mailing,
+        client=client,
+    )
+
+
+def update_status(mailing):
+    """Функция изменения статуса рассылки с учетом текущей даты"""
+    now = timezone.now()
+    if mailing.finished_at < now:
+        mailing.status_mail = "COMPLETED"
+    elif mailing.status_mail != "COMPLETED":
+        mailing.status_mail = "STARTED"
+    mailing.save()
